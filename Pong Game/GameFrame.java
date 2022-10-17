079048db25a5407ce0b9994d86ea36df4a8262e1
Lakshay Tyagi <tyagilakshay119@gmail.com>
2022-10-17 09:22:47 +0530

    Add GameFrame.java

---
+++ Pong Game/GameFrame.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+
+
+public class GameFrame extends JFrame{
+
+    GamePanel panel;
+    GameFrame(){
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
