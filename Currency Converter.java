af671041ce09cf9499ce6e23361205516f4096d0
briyandyju09 <70208092+briyandyju09@users.noreply.github.com>
2022-10-20 09:39:11 +0400

    Currency Converter

--- Currency Converter.java
+++ Currency Converter.java
@@ -1,241 +1,132 @@
-/*
-
-* To change this template, choose Tools | Templates
-
-* and open the template in the editor.
-
-*/
-
-package com.exchange;
-
-import java.io.*;
-
-import java.net.*;
-
-import java.util.*;
-
-import javax.servlet.*;
-
-import javax.servlet.http.*;
-
-import java.io.InputStream;
-
-import java.net.*;
-
-import com.google.gson.*;
-
-/**
-
-*
-
-* @author pakallis
-
-*/
-
-classRecv
-
-{
-
-private String lhs;
-
-private String rhs;
-
-private String error;
-
-private String icc;
-
-public Recv(
-
-{
-
-}
-
-public String getLhs()
-
-{
-
-return lhs;
-
-}
-
-public String getRhs()
-
-{
-
-return rhs;
-
-}
-
-}
-
-public classConvertextendsHttpServlet {
-
-/**
-
-* Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-
-* @param request servlet request
-
-* @param response servlet response
-
-* @throws ServletException if a servlet-specific error occurs
-
-* @throws IOException if an I/O error occurs
-
-*/
-
-protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
-
-throws ServletException, IOException {
-
-String query = "";
-
-String amount = "";
-
-String curTo = "";
-
-String curFrom = "";
-
-String submit = "";
-
-String res = "";
-
-HttpSession session;
-
-resp.setContentType("text/html;charset=UTF-8");
-
-PrintWriter out = resp.getWriter();
-
-/*Read request parameters*/
-
-amount = req.getParameter("amount");
-
-curTo = req.getParameter("to");
-
-curFrom = req.getParameter("from");
-
-/*Open a connection to google and read the result*/
-
-try {
-
-query = "http://www.google.com/ig/calculator?hl=en&q=" + amount + curFrom + "=?" + curTo;
-
-URL url = new URL(query);
-
-InputStreamReader stream = new InputStreamReader(url.openStream());
-
-BufferedReader in = new BufferedReader(stream);
-
-String str = "";
-
-String temp = "";
-
-while ((temp = in.readLine()) != null) {
-
-str = str + temp;
-
-}
-
-/*Parse the result which is in json format*/
-
-Gson gson = new Gson();
-
-Recv st = gson.fromJson(str, Recv.class);
-
-String rhs = st.getRhs();
-
-rhs = rhs.replaceAll("�", "");
-
-/*we do the check in order to print the additional word(millions,billions etc)*/
-
-StringTokenizer strto = new StringTokenizer(rhs);
-
-String nextToken;
-
-out.write(strto.nextToken());
-
-nextToken = strto.nextToken();
-
-if( nextToken.equals("million") || nextToken.equals("billion") || nextToken.equals("trillion"))
-
-{
-
-out.println(" "+nextToken);
-
-}
-
-} catch (NumberFormatException e) {
-
-out.println("The given amount is not a valid number");
-
-}
-
-}
-
-// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-
-/**
-
-* Handles the HTTP <code>GET</code> method.
-
-* @param request servlet request
-
-* @param response servlet response
-
-* @throws ServletException if a servlet-specific error occurs
-
-* @throws IOException if an I/O error occurs
-
-*/
-
-@Override
-
-protected void doGet(HttpServletRequest request, HttpServletResponse response)
-
-throws ServletException, IOException {
-
-processRequest(request, response);
-
-}
-
-/**
-
-* Handles the HTTP <code>POST</code> method.
-
-* @param request servlet request
-
-* @param response servlet response
-
-* @throws ServletException if a servlet-specific error occurs
-
-* @throws IOException if an I/O error occurs
-
-*/
-
-@Override
-
-protected void doPost(HttpServletRequest request, HttpServletResponse response)
-
-throws ServletException, IOException {
-
-processRequest(request, response);
-
-}
-
-/**
-
-* Returns a short description of the servlet.
-
-* @return a String containing servlet description
-
-*/
-
-@Override
-
-public String getServletInfo() {
-
-return "Short description";
-
-}// </editor-fold>
-
-}
+// Java program to convert from 
+// rupee to the dollar and vice-versa 
+// using Java Swing 
+
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; 
+public class GFG { 
+
+	// Function to convert from rupee 
+	// to the dollar and vice-versa 
+	// using Java Swing 
+	public static void converter() 
+	{ 
+
+		// Creating a new frame using JFrame 
+		JFrame f = new JFrame("CONVERTER"); 
+
+		// Creating two labels 
+		JLabel l1, l2; 
+
+		// Creating two text fields. 
+		// One for rupee and one for 
+		// the dollar 
+		JTextField t1, t2; 
+
+		// Creating three buttons 
+		JButton b1, b2, b3; 
+
+		// Naming the labels and setting 
+		// the bounds for the labels 
+		l1 = new JLabel("Rupees:"); 
+		l1.setBounds(20, 40, 60, 30); 
+		l2 = new JLabel("Dollars:"); 
+		l2.setBounds(170, 40, 60, 30); 
+
+		// Initializing the text fields with 
+		// 0 by default and setting the 
+		// bounds for the text fields 
+		t1 = new JTextField("0"); 
+		t1.setBounds(80, 40, 50, 30); 
+		t2 = new JTextField("0"); 
+		t2.setBounds(240, 40, 50, 30); 
+
+		// Creating a button for INR, 
+		// one button for the dollar 
+		// and one button to close 
+		// and setting the bounds 
+		b1 = new JButton("INR"); 
+		b1.setBounds(50, 80, 60, 15); 
+		b2 = new JButton("Dollar"); 
+		b2.setBounds(190, 80, 60, 15); 
+		b3 = new JButton("close"); 
+		b3.setBounds(150, 150, 60, 30); 
+
+		// Adding action listener 
+		b1.addActionListener(new ActionListener() { 
+			public void actionPerformed(ActionEvent e) 
+			{ 
+				// Converting to double 
+				double d 
+					= Double.parseDouble(t1.getText()); 
+
+				// Converting rupees to dollars 
+				double d1 = (d / 65.25); 
+
+				// Getting the string value of the 
+				// calculated value 
+				String str1 = String.valueOf(d1); 
+
+				// Placing it in the text box 
+				t2.setText(str1); 
+			} 
+		}); 
+
+		// Adding action listener 
+		b2.addActionListener(new ActionListener() { 
+			public void actionPerformed(ActionEvent e) 
+			{ 
+				// Converting to double 
+				double d2 
+					= Double.parseDouble(t2.getText()); 
+
+				// converting Dollars to rupees 
+				double d3 = (d2 * 65.25); 
+
+				// Getting the string value of the 
+				// calculated value 
+				String str2 = String.valueOf(d3); 
+
+				// Placing it in the text box 
+				t1.setText(str2); 
+			} 
+		}); 
+
+		// Action listener to close the form 
+		b3.addActionListener(new ActionListener() { 
+			public void actionPerformed(ActionEvent e) 
+			{ 
+				f.dispose(); 
+			} 
+		}); 
+
+		// Default method for closing the frame 
+		f.addWindowListener(new WindowAdapter() { 
+			public void windowClosing(WindowEvent e) 
+			{ 
+				System.exit(0); 
+			} 
+		}); 
+
+		// Adding the created objects 
+		// to the form 
+		f.add(l1); 
+		f.add(t1); 
+		f.add(l2); 
+		f.add(t2); 
+		f.add(b1); 
+		f.add(b2); 
+		f.add(b3); 
+
+		f.setLayout(null); 
+		f.setSize(400, 300); 
+		f.setVisible(true); 
+	} 
+
+	// Driver code 
+	public static void main(String args[]) 
+	{ 
+		converter(); 
+	} 
+} 
