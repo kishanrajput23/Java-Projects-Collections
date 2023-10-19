257bca92fff99610f3a72ad1c79bcee15355a1d0
Ayush varshney <ayushvarshney42003@gmail.com>
2023-10-19 15:24:10 +0530

    Exam Online

--- FileUploadingJS/php/upload.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $file_name =  $_FILES['file']['name']; //getting file name
-  $tmp_name = $_FILES['file']['tmp_name']; //getting temp_name of file
-  $file_up_name = time().$file_name; //making file name dynamic by adding time before file name
-  move_uploaded_file($tmp_name, "files/".$file_up_name); //moving file to the specified folder with dynamic name
-?>
