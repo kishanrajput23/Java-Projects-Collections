8dfac05d8482162776c0f19ccf658003e390e0d8
Atharva Meher <92387222+atharvam2004@users.noreply.github.com>
2023-10-19 10:57:24 +0530

    Create number.py
    made a passwrord generator

---
+++ number.py
@@ -0,0 +1,108 @@
+
+import random
+import string
+
+def generate_password(length=12, include_digits=True, include_special_chars=True):
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_special_chars:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = 16
+    use_digits = True
+    use_special_chars = True
+
+    password = generate_password(password_length, use_digits, use_special_chars)
+    print(f"Random Password: {password}")
+
+def generaword(length=12, include_digits=True, include_special_chars=True):
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_special_chars:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = 16
+    use_digits = True
+    use_special_chars = True
+
+    password = generate_password(password_length, use_digits, use_special_chars)
+    print(f"Random Password: {password}")
+
+def generate(length=12, include_digits=True, include_special_chars=True):
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_special_chars:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = 16
+    use_digits = True
+    use_special_chars = True
+
+    password = generate_password(password_length, use_digits, use_special_chars)
+    print(f"Random Password: {password}")
+def generate_password(length=12, include_digits=True, include_special_chars=True):
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_special_chars:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = 16
+    use_digits = True
+    use_special_chars = True
+
+    password = generate_password(password_length, use_digits, use_special_chars)
+    print(f"Random Password: {password}")
+def genssword(length=12, include_digits=True, include_special_chars=True):
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_special_chars:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = 16
+    use_digits = True
+    use_special_chars = True
+
+    password = generate_password(password_length, use_digits, use_special_chars)
+    print(f"Random Password: {password}")
+def gepassword(length=12, include_digits=True, include_special_chars=True):
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_special_chars:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = 16
+    use_digits = True
+    use_special_chars = True
+
+    password = generate_password(password_length, use_digits, use_special_chars)
+    print(f"Random Password: {password}")
