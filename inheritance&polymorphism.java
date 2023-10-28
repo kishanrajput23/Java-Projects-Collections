f809aeb783e25b4f91115123d9f167c673dc9415
Yachika9925 <147185379+Yachika9925@users.noreply.github.com>
2023-10-28 07:46:04 +0530

    Create inheritance&polymorphism.java
    pls review it and merge it

---
+++ inheritance&polymorphism.java
@@ -0,0 +1,27 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class AnimalDemo {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myDog.sound();
+        myCat.sound();
+    }
+}
