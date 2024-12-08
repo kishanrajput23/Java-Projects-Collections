16b7e8ac56849bda2ed037cde389b4dffd4b9037
Pranav <pran9v@gmail.com>
2024-12-08 13:06:27 +0530

    remove and view orders

--- OrderBook/src/main/orderbook/OrderBook.java
+++ OrderBook/src/main/orderbook/OrderBook.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class OrderBook {
@@ -19,8 +20,30 @@ public class OrderBook {
         orderMap.put(order.getId(), order);
     }
     
-    //remove Order
+    public boolean removeOrders(String orderId) {
+        Order order = orderMap.remove(orderId);
+        if(order==null) {
+            return false;
+        }
+        TreeMap<Double, List<Order>> refTreeMap = order.getIsBuyOrder() ? buyOrders : sellOrders;
+        List<Order> ordersAtPrice = refTreeMap.get(order.getPrice());
+        ordersAtPrice.remove(order);
+        if(ordersAtPrice.isEmpty()) {
+            refTreeMap.remove(order.getPrice());
+        }
+        return true;
+    }
     
-    //view Orders
+    public void viewOrder() {
+        System.out.println("Buy Orders are: ");
+        for(Map.Entry<Double, List<Order>> entry : buyOrders.entrySet()) {
+            System.out.println("Price: "+entry.getKey()+" -> "+entry.getValue());
+        }
+
+        System.out.println("Sell Order are: ");
+        for(Map.Entry<Double, List<Order>> entry : sellOrders.entrySet()) {
+            System.out.println("Price: "+entry.getKey()+" -> "+entry.getValue());
+        }
+    }
 
 }
