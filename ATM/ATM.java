f3e0d6928f41799970c37f5d77b27df28c9adf00
SR PRAWIN RAJA <srprawinraja@gmail.com>
2025-07-08 12:15:43 +0530

    added command lines to ATM

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,7 +1,6 @@
-
-public class ATM extends OptionMenu {
-    public static void main(String[] args) {
-        OptionMenu options = new OptionMenu();
-        options.getLogin();
+public class ATM extends OptionMenu { // Declares ATM class that inherits from OptionMenu
+    public static void main(String[] args) { // Main method: entry point of the program
+        OptionMenu options = new OptionMenu(); // Creates an object of OptionMenu class
+        options.getLogin(); // Calls the getLogin method to prompt user login
     }
 }
