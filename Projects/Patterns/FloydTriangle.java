f63dd5cc768c140bc69c98c1507ecb3e9e1619ff
Rohit <root1@Mac.local>
2026-04-19 07:31:44 +0530

    Added FloydTriangle Pattern

---
+++ Projects/Patterns/FloydTriangle.java
@@ -0,0 +1,14 @@
+public class FloydTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int num = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
