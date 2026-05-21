a25b6ab90f79236c09501a2b59c5a0f7134e5b32
num0001 <josiahwaterman209@gmail.com>
2026-05-21 12:15:03 -0400

    Add QuickSort algorithm in Projects/Algorithms

---
+++ Projects/Algorithms/QuickSort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] data = { 10, 7, 8, 9, 1, 5, -3, 22, 0 };
+
+        System.out.println("Before: " + Arrays.toString(data));
+        sort(data);
+        System.out.println("After:  " + Arrays.toString(data));
+    }
+}
