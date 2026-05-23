93eb6fdcdc819e9456d79a7733599a813afe5a97
Akepati Sathish Reddy <sathishreddyakepati@gmail.com>
2026-05-23 14:07:50 +0530

    testing change

--- Projects/Fibonacci/Fibonacci_series.java
+++ Projects/Fibonacci/Fibonacci_series.java
@@ -4,6 +4,7 @@ class Fibonacci_series {
     public static void main(String args[]) {
         int n1 = 0, n2 = 1, n3, i, count = 20;
         System.out.print(n1 + " " + n2);// printing 0 and 1
+        System.out.print("testing")
 
         for (i = 2; i < count; ++i)// loop starts from 2 because 0 and 1 are already printed
         {
